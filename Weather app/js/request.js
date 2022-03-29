4f82286d82d92201146d244734edfc4665de4106
AMIT KUMAR BERA <84179065+itzwow@users.noreply.github.com>
2022-03-29 07:59:55 +0530

    U

--- Weather app/js/request.js
+++ Weather app/js/request.js
@@ -1,11 +1,11 @@
 const key = 'cbe3dd267a18f6c89943b3eff94f1ed7';
 
-// const baseURL = 'http://api.lopenweathermap.org/data/2.5/weather?q=Kolkata&appid=cbe3dd267a18f6c89943b3eff94f1ed7';
+// const baseURL = 'https://api.lopenweathermap.org/data/2.5/weather?q=Kolkata&appid=cbe3dd267a18f6c89943b3eff94f1ed7';
 
 // fetch(baseURL).then((data)=>{console.log('response',data.json())})
 
 const requestCity = async (city) => {
-    const baseURL = 'http://api.openweathermap.org/data/2.5/weather'
+    const baseURL = 'https://api.openweathermap.org/data/2.5/weather'
     const query = `?q=${city}&appid=${key}`;
 
     //make fetch call (promise call)
